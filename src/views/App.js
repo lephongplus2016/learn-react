3da884e02e58994f702eb0e990bc270fda8dd044
lephongplus2016 <lephongplus2016@gmail.com>
2022-01-03 16:21:23 +0700

    form

--- src/views/App.js
+++ src/views/App.js
@@ -3,6 +3,7 @@ import "./App.scss";
 import MyComponent from "./example/MyComponent";
 import Bai5_state from "./example/Bai5_state";
 import Bai7_DOM_event from "./example/Bai7_DOM_event";
+import Bai9_form from "./example/Bai9_form";
 
 function App() {
     // cái đống html mà function này trả về, giới mộ điệu gọi là JSX
@@ -11,19 +12,12 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>Hello world</p>
-                {/* <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a> */}
                 {/* sau khi import thì component được sử dụng như 1 thẻ html */}
                 {/* <MyComponent></MyComponent> */}
                 {/* <MyComponent /> */}
                 {/* <Bai5_state /> */}
-                <Bai7_DOM_event />
+                {/* <Bai7_DOM_event /> */}
+                <Bai9_form />
             </header>
         </div>
     );
---
+++ src/views/example/Bai9_form.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+class Bai9_form extends React.Component {
+    // form không cần id, name vì quản lý qua state
+    // form không cần action vì thực hiện qua api
+    state = {
+        firstName: "",
+        lastName: "",
+    };
+    handleOnchangeFirstname = (event) => {
+        this.setState({
+            firstName: event.target.value,
+        });
+    };
+    handleOnchangeLastname = (event) => {
+        this.setState({
+            lastName: event.target.value,
+        });
+    };
+    handleClick = (event) => {
+        // ngăn mặc định sẽ tải lại trang mỗi khi thực hiện submit form
+        event.preventDefault();
+        console.log("check input data", this.state);
+    };
+    // mọi hàm gọi trong render đều phải qua this
+    render() {
+        return (
+            <>
+                {/* đối với form của react thì không cần action
+                yêu cầu mỗi thẻ br, label phải có / ở cuối */}
+                <form>
+                    <label for="fname">First name:</label>
+                    <br />
+                    <input
+                        type="text"
+                        value={this.state.firstName}
+                        onChange={(event) => {
+                            this.handleOnchangeFirstname(event);
+                        }}
+                    />
+                    <br />
+                    <label for="lname">Last name:</label>
+                    <br />
+                    <input
+                        type="text"
+                        value={this.state.lastName}
+                        onChange={(event) => {
+                            this.handleOnchangeLastname(event);
+                        }}
+                    />
+                    <br />
+                    <br />
+                    <input
+                        type="submit"
+                        value="Submit"
+                        onClick={(event) => this.handleClick(event)}
+                    />
+                </form>
+            </>
+        );
+    }
+}
+
+export default Bai9_form;
