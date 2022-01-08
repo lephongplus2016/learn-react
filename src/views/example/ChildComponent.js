0c98be1bdd6727e19d77c953a2aa80dc222b7562
lephongplus2016 <lephongplus2016@gmail.com>
2022-01-08 18:06:51 +0700

    cau lenh if trong react part 2

--- src/views/example/ChildComponent.js
+++ src/views/example/ChildComponent.js
@@ -1,28 +1,5 @@
 import React from "react";
 
-// khác với class, function component thì phải có parameter truyền vào
-// const ChildComponent = (props) => {
-//     let { arrJobs } = props;
-//     let aList;
-//     return (
-//         <>
-//             <div className="job-lists">
-//                 {
-//                     (aList = arrJobs.map((item, index) => {
-//                         if (item.salary >= 500) {
-//                             return (
-//                                 <div key={item.id}>
-//                                     {item.title} - {item.salary} $
-//                                 </div>
-//                             );
-//                         }
-//                     }))
-//                 }
-//                 {console.log("hàm map trả về cho ta cái gì", aList)}
-//             </div>
-//         </>
-//     );
-// };
 
 class ChildComponent extends React.Component {
     state = {
@@ -38,28 +15,31 @@ class ChildComponent extends React.Component {
     // chú ý: bản chất là dùng câu điều kiện if - {viết js trong jsx}
     //<> và </> để bao vô hình bao khối div lại thành 1 khối html để chạy điều kiện if showJob
     // react cho phép viết tắt if như vậy
+    // gọn hơn nữa dùng tam đoạn luận
     render() {
         let { arrJobs } = this.props;
         let aList;
         let showJob = this.state.showJob;
+        let check = showJob === true ? 'showJob === true' : 'showJob=== false';
         return (
             <>
-                { showJob=== false && <div><button onClick={()=> { this.handleShow()}}>show</button></div>}
-                { showJob === true &&
-                    <>
-                        <div className="job-lists">
-                        {
-                            (aList = arrJobs.map((item, index) => {
-                                    return (
-                                        <div key={item.id}>
-                                            {item.title} - {item.salary} $
-                                        </div>
-                                    );
-                            }))
-                        }
-                        </div>
-                        <div><button  onClick={()=> { this.handleShow()}}>hide</button></div>
-                    </>
+                { showJob === false ? 
+                        <div><button onClick={()=> { this.handleShow()}}>show</button></div>
+                    :
+                        <>
+                            <div className="job-lists">
+                            {
+                                (aList = arrJobs.map((item, index) => {
+                                        return (
+                                            <div key={item.id}>
+                                                {item.title} - {item.salary} $
+                                            </div>
+                                        );
+                                }))
+                            }
+                            </div>
+                            <div><button  onClick={()=> { this.handleShow()}}>hide</button></div>
+                        </>
                 }
 
             </>
